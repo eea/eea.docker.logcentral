430731d8648a8da940bab36ae55fcff233b9e637
Vitalie Maldur <maldur.vitalik@gmail.com>
2015-06-09 17:16:23 +0300

    Refs #21610 - Secret file for auth credentials

--- rabbitconfig/config.sh
+++ rabbitconfig/config.sh
@@ -4,17 +4,17 @@
 while ! curl --silent --max-time 3 "${BROKER}:15672/api"; do sleep 3; done
 
 # Delete any old exchange
-curl -XDELETE --silent -u 'guest:guest' \
+curl -XDELETE --silent -u "${USER}:${PASS}" \
     "${BROKER}:15672/api/exchanges/%2F/logging.gelf"
 
 # Create the exchange logging.gelf exchange
-curl -XPUT --silent -H 'content-type:application/json' -u 'guest:guest' \
+curl -XPUT --silent -H 'content-type:application/json' -u "${USER}:${PASS}" \
     "${BROKER}:15672/api/exchanges/%2F/logging.gelf" \
     -d '{"type": "fanout", "durable": true}'
 echo "Created exchange"
 
 # Bind it to the log-messages queue
-curl -XPOST --silent -H 'content-type:application/json' -u 'guest:guest' \
+curl -XPOST --silent -H 'content-type:application/json' -u "${USER}:${PASS}" \
     "${BROKER}:15672/api/bindings/%2F/e/logging.gelf/q/log-messages/" \
     -d '{"routing_key":"#", "arguments":{}}'
 echo "Created binding"
