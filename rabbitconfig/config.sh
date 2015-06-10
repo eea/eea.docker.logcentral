e8779bd47872c6d512863e0a474430f3503cbecb
Vitalie Maldur <maldur.vitalik@gmail.com>
2015-06-10 14:33:08 +0300

    Refs #21610 - Create RabbitMQ queue at startup

--- rabbitconfig/config.sh
+++ rabbitconfig/config.sh
@@ -13,6 +13,12 @@ curl -XPUT --silent -H 'content-type:application/json' -u "${USER}:${PASS}" \
     -d '{"type": "fanout", "durable": true}'
 echo "Created exchange"
 
+# Create queue
+curl -XPUT --silent -H 'content-type:application/json' -u "${USER}:${PASS}" \
+    "${BROKER}:15672/api/queues/%2F/log-messages" \
+    -d '{"auto_delete":false,"durable":true,"arguments":[]}'
+echo "Created queue"
+
 # Bind it to the log-messages queue
 curl -XPOST --silent -H 'content-type:application/json' -u "${USER}:${PASS}" \
     "${BROKER}:15672/api/bindings/%2F/e/logging.gelf/q/log-messages/" \
